e9cf441573b656ff2c735816d9c93193b9e7459a
USER <USER@DESKTOP-RL83N59>
2018-05-31 16:08:50 +0300

    recoded Prescription

--- src/main/frmPrescription.java
+++ src/main/frmPrescription.java
@@ -4,9 +4,6 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -27,18 +24,18 @@ import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
-public class frmMedicalCertificate extends javax.swing.JFrame {
+public class frmPrescription extends javax.swing.JFrame {
 DbConnection DbConn = new DbConnection();
 
-    public frmMedicalCertificate() {
+    public frmPrescription() {
         initComponents();
         DbConn.DoConnect();
-        FillTable();
+         FillTable();
         setLocationRelativeTo(null);
         setDefaultCloseOperation(frmMedicalCertificate.DISPOSE_ON_CLOSE);
-        tblMedCert.setAutoscrolls(true);
-        tblMedCert.setDefaultEditor(Object.class, null);
-        tblMedCert.setAutoResizeMode(tblMedCert.AUTO_RESIZE_OFF);
+        tblPrescription.setAutoscrolls(true);
+        tblPrescription.setDefaultEditor(Object.class, null);
+        tblPrescription.setAutoResizeMode(tblPrescription.AUTO_RESIZE_OFF);
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e){
@@ -61,32 +58,29 @@ DbConnection DbConn = new DbConnection();
         Logger.getLogger(frmLogin.class.getName()).log(Level.SEVERE, null, ex);
     }
     }
-    private void SetTableHeader(){
-        tblMedCert.getColumnModel().getColumn(0).setHeaderValue("PID");
-        tblMedCert.getColumnModel().getColumn(1).setHeaderValue("APPT ID");
-        tblMedCert.getColumnModel().getColumn(2).setHeaderValue("NAME");
-        tblMedCert.getColumnModel().getColumn(3).setHeaderValue("APPT DATE");
-        tblMedCert.getColumnModel().getColumn(4).setHeaderValue("DISCHARGED");
-        tblMedCert.getColumnModel().getColumn(5).setHeaderValue("DAYS");
-        tblMedCert.getColumnModel().getColumn(6).setHeaderValue("DIAGNOSIS");
-        tblMedCert.getColumnModel().getColumn(7).setHeaderValue("DOCTOR");
+ private void SetTableHeader(){
+        tblPrescription.getColumnModel().getColumn(0).setHeaderValue("NAME");
+        tblPrescription.getColumnModel().getColumn(1).setHeaderValue("PID");
+        tblPrescription.getColumnModel().getColumn(2).setHeaderValue("APPT ID");
+        tblPrescription.getColumnModel().getColumn(3).setHeaderValue("APPT DATE");
+        tblPrescription.getColumnModel().getColumn(4).setHeaderValue("DOCTOR");
+        tblPrescription.getColumnModel().getColumn(5).setHeaderValue("DESCRIPTION");
         
         int i=0;
-        while (i<tblMedCert.getColumnCount()){
-            tblMedCert.getColumnModel().getColumn(i).setPreferredWidth(100);
+        while (i<tblPrescription.getColumnCount()){
+            tblPrescription.getColumnModel().getColumn(i).setPreferredWidth(100);
             i++;
         }
     }
     private void ListenSearch(){
         try{
-            DbConn.SQLQuery = "select * from tblmedicalcertificate where mc_patientname like ? or mc_pid like ? ";
+            DbConn.SQLQuery = "select pm_name,pm_pid,pm_apptid,pm_adddate,pm_doctor,pm_description from tblpatientmedications where pm_name like ? or pm_pid like ? ";
             DbConn.pstmt = DbConn.conn.prepareStatement(DbConn.SQLQuery);
             DbConn.pstmt.setString(1, "%" + txtSearch.getText() + "%");
             DbConn.pstmt.setString(2, "%" + txtSearch.getText() + "%");
             DbConn.rs = DbConn.pstmt.executeQuery();
-            tblMedCert.setModel(DbUtils.resultSetToTableModel(DbConn.rs));
+            tblPrescription.setModel(DbUtils.resultSetToTableModel(DbConn.rs));
             DbConn.pstmt.close();
-            tblMedCert.removeColumn(tblMedCert.getColumnModel().getColumn(0));
             SetTableHeader();
         }catch(SQLException e){
             JOptionPane.showMessageDialog(this, e.getMessage());
@@ -94,12 +88,11 @@ DbConnection DbConn = new DbConnection();
     }
     private void FillTable(){
         try{
-            DbConn.SQLQuery = "select * from tblmedicalcertificate order by mc_patientname";
+            DbConn.SQLQuery = "select pm_name,pm_pid,pm_apptid,pm_adddate,pm_doctor,pm_description from tblpatientmedications order by pm_name";
             DbConn.pstmt = DbConn.conn.prepareStatement(DbConn.SQLQuery);
             DbConn.rs = DbConn.pstmt.executeQuery();
-            tblMedCert.setModel(DbUtils.resultSetToTableModel(DbConn.rs));
+            tblPrescription.setModel(DbUtils.resultSetToTableModel(DbConn.rs));
             DbConn.pstmt.close();
-            tblMedCert.removeColumn(tblMedCert.getColumnModel().getColumn(0));
             SetTableHeader();
         }catch(SQLException e){
             JOptionPane.showMessageDialog(this, e.getMessage());
@@ -117,18 +110,12 @@ DbConnection DbConn = new DbConnection();
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        tblMedCert = new javax.swing.JTable();
+        tblPrescription = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
-        jPanel3 = new javax.swing.JPanel();
-        jPanel4 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        txtAreaDiagnosis = new javax.swing.JTextArea();
         jLabel2 = new javax.swing.JLabel();
         txtSearch = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Medical Certificate");
 
         jPanel1.setBackground(new java.awt.Color(214, 214, 194));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -137,8 +124,8 @@ DbConnection DbConn = new DbConnection();
         jPanel2.setBorder(javax.swing.BorderFactory.createMatteBorder(2, 2, 2, 2, new java.awt.Color(153, 153, 153)));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        tblMedCert.setBackground(new java.awt.Color(214, 214, 194));
-        tblMedCert.setModel(new javax.swing.table.DefaultTableModel(
+        tblPrescription.setBackground(new java.awt.Color(214, 214, 194));
+        tblPrescription.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -149,12 +136,12 @@ DbConnection DbConn = new DbConnection();
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        tblMedCert.addMouseListener(new java.awt.event.MouseAdapter() {
+        tblPrescription.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                tblMedCertMouseClicked(evt);
+                tblPrescriptionMouseClicked(evt);
             }
         });
-        jScrollPane1.setViewportView(tblMedCert);
+        jScrollPane1.setViewportView(tblPrescription);
 
         jPanel2.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 610, 167));
 
@@ -168,39 +155,7 @@ DbConnection DbConn = new DbConnection();
                 jButton1ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 400, -1, -1));
-
-        jPanel3.setBackground(new java.awt.Color(214, 214, 194));
-
-        jPanel4.setBackground(new java.awt.Color(214, 214, 194));
-        jPanel4.setBorder(javax.swing.BorderFactory.createMatteBorder(2, 2, 2, 2, new java.awt.Color(153, 153, 153)));
-        jPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel1.setText("Diagnosis:");
-        jPanel4.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 6, -1, -1));
-
-        txtAreaDiagnosis.setColumns(20);
-        txtAreaDiagnosis.setRows(5);
-        jScrollPane2.setViewportView(txtAreaDiagnosis);
-
-        jPanel4.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 600, 90));
-
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE)
-        );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-
-        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 260, 630, 140));
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 260, -1, -1));
 
         jLabel2.setForeground(new java.awt.Color(0, 0, 0));
         jLabel2.setText("Enter Name or PID:");
@@ -213,34 +168,35 @@ DbConnection DbConn = new DbConnection();
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 646, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 326, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void tblPrescriptionMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblPrescriptionMouseClicked
+        
+    }//GEN-LAST:event_tblPrescriptionMouseClicked
+
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        int row = tblMedCert.getSelectedRow();
-        int ba = tblMedCert.convertRowIndexToModel(row);
+        int row = tblPrescription.getSelectedRow();
+        int ba = tblPrescription.convertRowIndexToModel(row);
         Map param = new HashMap();
-        param.put("apptid", tblMedCert.getValueAt(ba, 1));
-//        param.put("dateTo", sdfDate.format(getToDate));
-//        param.put("showStatus", cmbStatus.getSelectedItem().toString());
-//        param.put("showClient", cmbClient.getSelectedItem().toString());
+        param.put("apptNumber", tblPrescription.getValueAt(ba, 2));
         try{
             DbConn.conn.close();
             Class.forName("com.mysql.jdbc.Driver");
             DbConn.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbemrph","root","root");
 //            DbConn.conn = DriverManager.getConnection("jdbc:mysql://166.62.10.53:3306/dbemrbeta","betapilfer","123456789");
-            JasperDesign jd = JRXmlLoader.load(new File("src\\reports\\reportMedCert.jrxml"));
+            JasperDesign jd = JRXmlLoader.load(new File("src\\reports\\reportPrescription.jrxml"));
             JasperReport jr = JasperCompileManager.compileReport(jd);
             JasperPrint jp = JasperFillManager.fillReport(jr, param,DbConn.conn);
             JasperViewer.viewReport(jp,false);
@@ -249,12 +205,6 @@ DbConnection DbConn = new DbConnection();
             JOptionPane.showMessageDialog(this, e.getMessage());
         }
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void tblMedCertMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblMedCertMouseClicked
-        int row = tblMedCert.getSelectedRow();
-        int ba = tblMedCert.convertRowIndexToModel(row);
-        txtAreaDiagnosis.setText(tblMedCert.getValueAt(ba, 6).toString());
-    }//GEN-LAST:event_tblMedCertMouseClicked
 
     /**
      * @param args the command line arguments
@@ -273,36 +223,31 @@ DbConnection DbConn = new DbConnection();
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frmMedicalCertificate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmPrescription.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frmMedicalCertificate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmPrescription.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frmMedicalCertificate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmPrescription.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmMedicalCertificate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmPrescription.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frmMedicalCertificate().setVisible(true);
+                new frmPrescription().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable tblMedCert;
-    private javax.swing.JTextArea txtAreaDiagnosis;
+    private javax.swing.JTable tblPrescription;
     private javax.swing.JTextField txtSearch;
     // End of variables declaration//GEN-END:variables
 }
